6582ac1cb85495158bd39283e3cadf51590ae346
munuhee <murichustephen@gmail.com>
2023-11-04 19:05:10 +0300

    work in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,27 @@ document.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
   /**
+   * Smooth scrooling
+   */
+  const links = document.querySelectorAll('a[href^="#"]');
+
+    for (const link of links) {
+        link.addEventListener("click", smoothScroll);
+    }
+
+    function smoothScroll(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: "smooth",
+            });
+        }
+    }
+
+  /**
    * Preloader
    */
   const preloader = document.querySelector("#preloader");
